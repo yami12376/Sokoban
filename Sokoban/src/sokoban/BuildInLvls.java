80af7fe0a1ea5a869b5fb0db94f99805ad48e764
kamil <Kamil_wit1@wp.pl>
2016-01-21 14:00:30 +0100

    wstepnie metoda do brania LVLi z katologu sokoban/lvls

--- Sokoban/src/sokoban/BuildInLvls.java
+++ Sokoban/src/sokoban/BuildInLvls.java
@@ -1,6 +1,11 @@
 package sokoban;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.FileUtils;
 
 public class BuildInLvls {
 	
@@ -40,7 +45,43 @@ public class BuildInLvls {
 	 BuildInLvls() {
 		 lista.add(lvl2);
 		 lista.add(lvl3);
+                 
+                  ArrayList<int[][]> lista = loadLvls();
 	 }
 	 
+             private ArrayList<int[][]> loadLvls()  {
+        ArrayList<int[][]> lista = new ArrayList<int[][]>();
+        String path = "";
+             File directory = new File (".");
+		 try {
+                path = directory.getCanonicalPath();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+                 
+                 // (path + "/graphics/gameElements/" +  name + ".png");
+        
+        File folder = new File(path + "/lvls");
+        File[] listOfFiles = folder.listFiles();
+
+        if (listOfFiles != null) {
+            
+      
+        for (int i = 0; i < listOfFiles.length; i++) {
+            File file = listOfFiles[i];
+            if (file.isFile() && file.getName().endsWith(".sokobanlevel")) {
+                try {
+                    String content = FileUtils.readFileToString(file);
+                    /* do somthing with content */
+                } catch (IOException ex) {
+                    Logger.getLogger(BuildInLvls.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+  }
+        return lista;
+    }
+
+         
 	 }
 
