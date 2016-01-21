1b0a5f0e8392a0051efeed69eac480c0b80d4f39
kamil <Kamil_wit1@wp.pl>
2016-01-21 15:01:23 +0100

    wczytywanie buffer reader od absolutnej scuiezki pliku

--- Sokoban/src/sokoban/BuildInLvls.java
+++ Sokoban/src/sokoban/BuildInLvls.java
@@ -1,6 +1,8 @@
 package sokoban;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -8,80 +10,81 @@ import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
 
 public class BuildInLvls {
-	
-	
+
 	// 0 - puste pola
-		// 1- bloki przez ktore nie mozemy przejsc, ani ich przesuwac
-		// 2 - pola na ktore kamienie maja trafic
-		// 3 - skrzynki, te ktore poruszamy
-		// 4 - to my
-		
-		int lvl2[][] = {
-				{1,1,1,1,1,1,1,1,1,1,1,1,0,0}, //0
-				{1,0,0,0,0,1,0,0,0,0,0,1,1,1}, //1
-				{1,0,0,0,0,1,0,3,0,0,3,0,0,1}, //2
-				{1,3,0,0,0,1,3,1,1,1,1,0,0,1}, //3
-				{1,2,0,0,0,3,0,4,0,1,1,0,0,1}, //4
-				{1,0,2,0,0,1,0,1,0,0,3,0,1,1}, //5
-				{1,1,1,1,1,1,0,1,1,3,0,3,0,1}, //6
-				{0,0,1,0,3,0,0,3,0,3,0,3,0,1}, //7
-				{0,0,1,0,0,0,0,1,0,0,0,0,0,1}, //8
-				{0,0,1,1,1,1,1,1,1,1,1,1,1,1}}; //9
-				
-				
-	int lvl3[][]=				{{1,1,1,1,1,1,1,1,1,1,1,1,0,0},
-				{1,0,0,0,0,1,0,0,0,0,0,1,1,1},
-				{1,0,0,0,0,1,0,3,0,0,3,0,0,1},
-				{1,0,0,0,0,1,3,1,1,1,1,0,0,1},
-				{1,0,0,0,0,3,0,4,0,1,1,0,0,1},
-				{1,0,2,0,0,1,0,1,0,0,3,0,1,1},
-				{1,1,1,1,1,1,0,1,1,3,0,3,0,1},
-				{0,0,1,0,3,0,0,3,0,3,0,3,0,1},
-				{0,0,1,0,0,0,0,1,0,0,0,0,0,1},
-				{0,0,1,1,1,1,1,1,1,1,1,1,1,1}	};
-		
-	 ArrayList<int[][]> lista = new ArrayList<int[][]>();
-	
-	 BuildInLvls() {
-		 lista.add(lvl2);
-		 lista.add(lvl3);
-                 
-                  ArrayList<int[][]> lista = loadLvls();
-	 }
-	 
-             private ArrayList<int[][]> loadLvls()  {
+    // 1- bloki przez ktore nie mozemy przejsc, ani ich przesuwac
+    // 2 - pola na ktore kamienie maja trafic
+    // 3 - skrzynki, te ktore poruszamy
+    // 4 - to my
+    int lvl2[][] = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, //0
+        {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1}, //1
+        {1, 0, 0, 0, 0, 1, 0, 3, 0, 0, 3, 0, 0, 1}, //2
+        {1, 3, 0, 0, 0, 1, 3, 1, 1, 1, 1, 0, 0, 1}, //3
+        {1, 2, 0, 0, 0, 3, 0, 4, 0, 1, 1, 0, 0, 1}, //4
+        {1, 0, 2, 0, 0, 1, 0, 1, 0, 0, 3, 0, 1, 1}, //5
+        {1, 1, 1, 1, 1, 1, 0, 1, 1, 3, 0, 3, 0, 1}, //6
+        {0, 0, 1, 0, 3, 0, 0, 3, 0, 3, 0, 3, 0, 1}, //7
+        {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1}, //8
+        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}}; //9
+
+    int lvl3[][] = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1},
+    {1, 0, 0, 0, 0, 1, 0, 3, 0, 0, 3, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1, 3, 1, 1, 1, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 3, 0, 4, 0, 1, 1, 0, 0, 1},
+    {1, 0, 2, 0, 0, 1, 0, 1, 0, 0, 3, 0, 1, 1},
+    {1, 1, 1, 1, 1, 1, 0, 1, 1, 3, 0, 3, 0, 1},
+    {0, 0, 1, 0, 3, 0, 0, 3, 0, 3, 0, 3, 0, 1},
+    {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+    {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+    ArrayList<int[][]> lista = new ArrayList<int[][]>();
+
+    BuildInLvls() {
+        lista.add(lvl2);
+        lista.add(lvl3);
+
+        ArrayList<int[][]> lista = loadLvls();
+    }
+
+    private ArrayList<int[][]> loadLvls() {
         ArrayList<int[][]> lista = new ArrayList<int[][]>();
         String path = "";
-             File directory = new File (".");
-		 try {
-                path = directory.getCanonicalPath();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-                 
+        File directory = new File(".");
+        try {
+            path = directory.getCanonicalPath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
                  // (path + "/graphics/gameElements/" +  name + ".png");
-        
         File folder = new File(path + "/lvls");
         File[] listOfFiles = folder.listFiles();
 
         if (listOfFiles != null) {
-            
-      
-        for (int i = 0; i < listOfFiles.length; i++) {
-            File file = listOfFiles[i];
-            if (file.isFile() && file.getName().endsWith(".sokobanlevel")) {
-                try {
-                    String content = FileUtils.readFileToString(file);
-                    /* do somthing with content */
-                } catch (IOException ex) {
-                    Logger.getLogger(BuildInLvls.class.getName()).log(Level.SEVERE, null, ex);
+
+            for (int i = 0; i < listOfFiles.length; i++) {
+                File file = listOfFiles[i];
+                if (file.isFile() && file.getName().endsWith(".sokobanlevel")) {
+                    try {
+                        BufferedReader in = new BufferedReader(new FileReader(file.getAbsolutePath()));
+
+                        String line;
+                        while ((line = in.readLine()) != null) {
+                            System.out.println(line);
+                        }
+                        in.close();
+
+                      //  String content = FileUtils.readFileToString(file);
+                        /* do somthing with content */
+                    } catch (IOException ex) {
+                        Logger.getLogger(BuildInLvls.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
             }
         }
-  }
         return lista;
     }
 
-         
-	 }
-
+}
